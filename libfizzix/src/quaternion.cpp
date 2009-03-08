d263b618dd64bc64379b00b991b33cf6412dd386
monkey.chess <monkey.chess@gmail.com>
2009-03-08 23:45:28 +0000

    Created a quaternion class.  People who know c++, please check that I did this right.

---
+++ libfizzix/src/quaternion.cpp
@@ -0,0 +1,129 @@
+/************************************************************************************************
+This file is a part of libfizzix backend to the Fizzix project.
+The website for this project is http://code.google.com/p/fizzix .
+Copyright (C) 2009
+	Joseph Lynch
+	Anton Frolenkov
+	Purnima Balakrishnan
+	Daniel Stiles
+	Eric Van Albert
+
+This program is free software; you can redistribute it and/or 
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+************************************************************************************************/
+
+#ifndef QUATERNION_CPP
+#define QUATERNION_CPP
+
+#include "quaternion.h"
+#include <math.h>
+
+Quaternion::Quaternion()
+{
+    values = new vector(4,0.0);
+    values[0] = 1.0;
+}
+
+Quaternion::Quaternion(*vector rot)
+{
+    values = rot;
+}
+
+Quaternion::Quaternion(*Quaternion toCopy)
+{
+    values = new vector(toCopy.getVec());
+}
+
+Quaternion::Quaternion(double w,double x,double y,double z)
+{
+    values = new vector(4,0.0);
+    values[0] = w;
+    values[1] = x;
+    values[2] = y;
+    values[3] = z;
+}
+
+Quaternion::~Quaternion()
+{
+    delete values;
+}
+
+*vector Quaternion::getVec()
+{
+    return values;
+}
+
+void Quaternion::setVec(*vector v)
+{
+    values = v;
+}
+
+void Quaternion::normalize(double tolerance)
+{
+    double magsquared = this->getVec()->at(0)*this->getVec()->at(0)+this->getVec()->at(1)*this->getVec()->at(1)+this->getVec()->at(2)*this->getVec()->at(2)+this->getVec()->at(3)*this->getVec()->at(3);
+    if (magsquared > tolerance*tolerance)
+    {
+        double mag = sqrt(magsquared);
+        this->getVec->at(0) /= mag;
+        this->getVec->at(1) /= mag;
+        this->getVec->at(2) /= mag;
+        this->getVec->at(3) /= mag;
+    }
+}
+
+Quaternion Quaternion::operator+(Quaternion& other)
+{
+    return new Quaternion(this->getVec()->at(0)+other->getVec()->at(0),this->getVec()->at(1)+other->getVec()->at(1),this->getVec()->at(2)+other->getVec()->at(2),this->getVec()->at(3)+other->getVec()->at(3));
+}
+
+void Quaternion::operator+=(Quaternion& other)
+{
+    this->getVec()->at(0)+=other->getVec()->at(0);
+    this->getVec()->at(1)+=other->getVec()->at(1);
+    this->getVec()->at(2)+=other->getVec()->at(2);
+    this->getVec()->at(3)+=other->getVec()->at(3);
+}
+
+Quaternion Quaternion::operator-(Quaternion& other)
+{
+    return new Quaternion(this->getVec()->at(0)-other->getVec()->at(0),this->getVec()->at(1)-other->getVec()->at(1),this->getVec()->at(2)-other->getVec()->at(2),this->getVec()->at(3)-other->getVec()->at(3));
+}
+
+void Quaternion::operator-=(Quaternion& other)
+{
+    this->getVec()->at(0)-=other->getVec()->at(0);
+    this->getVec()->at(1)-=other->getVec()->at(1);
+    this->getVec()->at(2)-=other->getVec()->at(2);
+    this->getVec()->at(3)-=other->getVec()->at(3);
+}
+
+Quaternion Quaternion::operator*(Quaternion& other)
+{
+    *Quaternion product = new Quaternion();
+    product->getVec()->at(0) = this->getVec()->at(0)*other->getVec()->at(0) - this->getVec()->at(1)*other->getVec()->at(1) - this->getVec()->at(2)*other->getVec()->at(2) - this->getVec()->at(3)*other->getVec()->at(3);
+    product->getVec()->at(1) = this->getVec()->at(0)*other->getVec()->at(0) + this->getVec()->at(1)*other->getVec()->at(1) + this->getVec()->at(2)*other->getVec()->at(2) - this->getVec()->at(3)*other->getVec()->at(3);
+    product->getVec()->at(2) = this->getVec()->at(0)*other->getVec()->at(0) - this->getVec()->at(1)*other->getVec()->at(1) + this->getVec()->at(2)*other->getVec()->at(2) + this->getVec()->at(3)*other->getVec()->at(3);
+    product->getVec()->at(3) = this->getVec()->at(0)*other->getVec()->at(0) + this->getVec()->at(1)*other->getVec()->at(1) - this->getVec()->at(2)*other->getVec()->at(2) + this->getVec()->at(3)*other->getVec()->at(3);
+    return *product;
+}
+
+void Quaternion::operator*=(Quaternion& other)
+{
+    this->getVec()->at(0) = this->getVec()->at(0)*other->getVec()->at(0) - this->getVec()->at(1)*other->getVec()->at(1) - this->getVec()->at(2)*other->getVec()->at(2) - this->getVec()->at(3)*other->getVec()->at(3);
+    this->getVec()->at(1) = this->getVec()->at(0)*other->getVec()->at(0) + this->getVec()->at(1)*other->getVec()->at(1) + this->getVec()->at(2)*other->getVec()->at(2) - this->getVec()->at(3)*other->getVec()->at(3);
+    this->getVec()->at(2) = this->getVec()->at(0)*other->getVec()->at(0) - this->getVec()->at(1)*other->getVec()->at(1) + this->getVec()->at(2)*other->getVec()->at(2) + this->getVec()->at(3)*other->getVec()->at(3);
+    this->getVec()->at(3) = this->getVec()->at(0)*other->getVec()->at(0) + this->getVec()->at(1)*other->getVec()->at(1) - this->getVec()->at(2)*other->getVec()->at(2) + this->getVec()->at(3)*other->getVec()->at(3);
+}
+
+#endif
---
+++ libfizzix/src/quaternion.h
@@ -0,0 +1,76 @@
+/************************************************************************************************
+This file is a part of libfizzix backend to the Fizzix project.
+The website for this project is http://code.google.com/p/fizzix .
+Copyright (C) 2009
+	Joseph Lynch
+	Anton Frolenkov
+	Purnima Balakrishnan
+	Daniel Stiles
+	Eric Van Albert
+
+This program is free software; you can redistribute it and/or 
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+************************************************************************************************/
+
+
+#ifndef QUATERNION_H
+#define QUATERNION_H
+
+#include <vector.h>
+
+/* This class defines a quaternion, with methods for multiplication, addition, and normalization. */
+class Quaternion
+{
+    private:
+        *vector<double> values; // (w,x,y,z)
+    public:
+        /* Default constructor
+         * Creates a unit quaternion with value (1,0,0,0). */
+        Quaternion();
+        /* Copy constructor */
+        Quaternion(*Quaternion);
+        /* Constructor with a vector of length 4. */
+        Quaternion(*vector)
+	/* Creates a quaternion with value (w,x,y,z) */
+        Quaternion(double w,double x,double y,double z);
+        /* Default destructor */
+        ~Quaternion();
+        /* Get a reference to this quaternion's vector */
+        vector getVec();
+        /* Set this quaternion's vector */
+        void setVec(*vector);
+        /* Normalize this quaternion
+           Automatically checks that it is unit within tolerance,
+             and will not change if it is close enough */
+        void normalize(double tolerance);
+        /* Return the sum of this and another quaternion */
+        Quaternion operator+(Quaternion&);
+        /* Add another quaternion to this one */
+        void operator+=(Quaternion&);
+        /* Return the difference of this and another quaternion */
+        Quaternion operator-(Quaternion&);
+        /* Subtract another quaternion from this one */
+        void operator-=(Quaternion&);
+        /* Return the product of this and another quaternion
+           wnew = w1*w2 - x1*x2 - y1*y2 - z1*z2
+           xnew = w1*w2 + x1*x2 + y1*y2 - z1*z2
+           ynew = w1*w2 - x1*x2 + y1*y2 + z1*z2
+           znew = w1*w2 + x1*x2 - y1*y2 + z1*z2 */
+        Quaternion operator*(Quaternion&);
+        /* Multiply another quaternion on this one on the left
+           qnew = other * q */
+        void operator*=(Quaternion&);
+};
+
+#endif
